458dfb04c7a26a49ea7399b2d464190dfe276f9f
flawnson <flawnsontong1@gmail.com>
2020-06-14 18:52:22 -0400

    First commit for generic GNN pipeline with DGL

---
+++ main.py
@@ -0,0 +1,26 @@
+import os.path as osp
+import argparse
+import torch
+import json
+
+from read.preprocessing import GenericDataset, PrimaryLabelset
+
+
+if __name__ == "__main__":
+    # TODO: Implement logger to log progress of code execution
+    # TODO: Create directory tree and requirements.txt with bash script
+    # TODO: Add __init__.py files to each project directory
+
+    path = osp.join('data', 'biogrid')
+    parser = argparse.ArgumentParser(description="Config file parser")
+    parser.add_argument("-c", "--config", help="json config file", type=str)
+    parser.add_argument("-b", "--benchmarking", help="benchmarking run config", type=str)
+    args = parser.parse_args()
+    bm = args.benchmarking
+
+    json_data: dict = json.load(open(args.config))
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    dataset = PrimaryLabelset(json_data["data_config"])
+    print(dataset)
+
---
+++ read/preprocessing.py
@@ -0,0 +1,108 @@
+import networkx as nx
+import os.path as osp
+import pandas as pd
+import numpy as np
+import torch as tg
+import json
+import dgl
+
+from abc import abstractmethod, ABC
+
+
+class GenericDataset(ABC):
+    """
+    Abstract base class from which subclasses will inherit from.
+    Currently the intended use is for datasets with multiple labelsets.
+    Modifications can be made to compensate for multiple graph edgelists, node features, etc.
+    """
+    @abstractmethod
+    def __init__(self, config):
+        super(GenericDataset, self).__init__()
+        self.data_config = config
+        self.dataset = self.preprocessing()
+
+    def get_edges(self):
+        edgelist_path = osp.join(osp.dirname(osp.dirname(__file__)),
+                                 osp.join(*self.data_config["directory"]),
+                                 self.data_config["edgelist_file"])
+
+        edgeframe = pd.read_csv(edgelist_path, header=0, index_col=False)
+
+        edges = nx.readwrite.edgelist.read_edgelist(edgelist_path)
+
+        return edges
+
+    @staticmethod
+    def get_edge_features():
+        # TODO: Add adapter (put in utils dir) for csv, json, and txt files
+        # TODO: Make edge and node features optional (and implement for dummy)
+        pass
+
+    def get_node_features(self):
+        features_dict = json.load(open(osp.join(osp.dirname(osp.dirname(__file__)),
+                                                *self.data_config["directory"],
+                                                self.data_config["features_file"])))
+
+        features_dict = {name: embed for name, embed
+                         in zip(features_dict["gene"].values(),
+                                features_dict["embeddings"].values())}
+
+        if self.data_config["dummy_features"]:
+            features_dict = {key: np.ones(self.data_config["dummy_features"], dtype=np.double) for key, value
+                             in features_dict.items()}
+
+        return features_dict
+
+    def get_targets(self):
+        node_labels = pd.read_csv(osp.join(osp.dirname(osp.dirname(__file__)),
+                                           *self.data_config["directory"],
+                                           self.data_config["label_file"]), header=0)
+
+        target_data = [node_labels[name].tolist() for name in self.data_config["label_names"]]
+
+        return self.get_labels(target_data)
+
+    @abstractmethod
+    def get_labels(self, target_data):
+        # Must be implemented by subclasses. File reading logic is held in get_targets()
+        return {}
+
+    def intersection(self):
+        # TODO: Logic for intersecting edges, nodes, features, labels, etc
+
+        nx_graph: nx.graph = self.get_edges()
+        target_data: dict = self.get_targets()
+
+        # Needed to compensate for differences between targetset and edgelist (asign nodes without labels to unknown)
+        target_data = {name: 0 if name not in target_data else target_data[name] for name in nx_graph.nodes()}
+
+        nx.set_node_attributes(nx_graph, target_data, "y")
+        nx.set_node_attributes(nx_graph, self.get_node_features(), "x")
+
+        # Filter for nodes with embeddings
+        [nx_graph.remove_node(n) for (n, d) in nx_graph.copy().nodes(data=True) if "x" not in d]
+
+        return nx_graph
+
+    def preprocessing(self):
+        # TODO: Logic for turning data objects into library-specific implementations
+        nx_graph = self.intersection()
+        dgl_graph = dgl.DGLGraph().from_networkx(nx_graph)
+
+        return dgl_graph
+
+
+class PrimaryLabelset(GenericDataset, ABC):
+    def __init__(self, config: dict):
+        super(PrimaryLabelset, self).__init__(config=config)
+
+    @staticmethod
+    def extract_labels(target_data):
+        # Labels starts from 1, since 0 is reserved for unknown class in the case of semi-supervised learning
+        # Can manually create dictionary that maps from data to integer
+        # Note that PyG automatically turns ints into onehot
+        return dict(zip(np.unique(target_data[1]), list(range(1, len(np.unique(target_data[1]))))))
+
+    def get_labels(self, target_data) -> dict:
+        # Abstract method defined in GenericDataset
+        return {name: self.extract_labels(target_data) for name, label in zip(target_data[0], target_data[1])}
