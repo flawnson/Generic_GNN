534b9930884d056db8667e6bdaf5b19e9d4c7413
flawnson <flawnsontong1@gmail.com>
2020-06-19 13:04:37 -0400

    Continued work on DGL Layer and model

--- nn/DGL_layers.py
+++ nn/DGL_layers.py
@@ -8,26 +8,47 @@ import torch.nn.functional as F
 
 class GNNLayer(nn.Module):
     # XXX: Reference for self; u: source node, v: destination node, e edges among those nodes
-    def __init__(self, in_size, out_size, bias, activation, **kwargs):
+    def __init__(self, in_size, out_size, activation, weight=True, bias=True, **kwargs):
         super(GNNLayer, self).__init__()
-        self.linear = nn.Linear(in_size, out_size)
+        self._activation = activation
+        self.linear = nn.Linear(in_size, out_size, bias=bias)
         self.bias: bool = bias
-        self.activation = activation
+
+        if weight:
+            self.weight = nn.Parameter(torch.Tensor(in_size, out_size), requires_grad=True)
+        else:
+            self.register_parameter('weight', None)
 
     def func_factory(self):
-        func = fn.
-        return func
+        # func = fn.
+        return None
 
-    def forward(self, graph_obj, feature):
+    def reset_parameters(self):
+        # Obligatory parameter reset method
+        pass
+
+    def forward(self, graph_obj, feature, weight=True):
         # local_scope needed to ensure that the stored data (in messages) doesn't accumulate
         with graph_obj.local_scope():
-            graph_obj.ndata["feature_name"]
+            if self._norm == 'both':
+                degs = graph_obj.out_degrees().to(feature.device).float().clamp(min=1)
+                norm = torch.pow(degs, -0.5)
+                shp = norm.shape + (1,) * (feature.dim() - 1)
+                norm = torch.reshape(norm, shp)
+                feat = feature * norm
+
+            if weight is not None:
+                if self.weight is not None:
+                    raise AssertionError('External weight is provided while at the same time the'
+                                         ' module has defined its own weight parameter. Please'
+                                         ' create the module with flag weight=False.')
+            else:
+                weight = self.weight
+
+            graph_obj.ndata["feature_name"] = feature
             graph_obj.update_all()
-            graph_obj.send_and_recv()
-
             h = graph_obj.ndata["h"]
-
             if self.activation is not None:
+                h = self._activation(h)
 
-
-            return self.linear(h)
+        return self.linear(h)
