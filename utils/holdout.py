690b1baec857ab18187d6235b4590cf432c797e3
flawnson <flawnsontong1@gmail.com>
2020-06-30 00:30:40 -0400

    Cleaned up and completed splitting mechanism

--- utils/holdout.py
+++ utils/holdout.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 
-from dgl.data.utils import Subset, split_dataset
+from dgl.data.utils import Subset
 from itertools import accumulate
 
 
@@ -11,29 +11,19 @@ class Holdout:
         self.data_config = data_config
         self.dataset = dataset
 
-    def tri_split(self):
-        splits = split_dataset(self.dataset, frac_list=self.data_config["split_sizes"],
-                               shuffle=self.data_config["shuffle"])
-
-        return splits
-
     def split(self):
-        if self.data_config["split_sizes"] is None:
-            frac_list = [0.8, 0.1, 0.1]
-        else:
-            frac_list = self.data_config["split_sizes"]
+        frac_list = self.data_config.get("split_sizes", [0.8, 0.1, 0.1])
 
         frac_list = np.asarray(frac_list)
-        assert np.allclose(np.sum(frac_list), 1.), \
-            'Expect frac_list sum to 1, got {:.4f}'.format(np.sum(frac_list))
+        assert np.allclose(np.sum(frac_list), 1.), 'Expected frac_list to sum to 1, got {:.4f}'.format(
+            np.sum(frac_list))
 
         num_data = len(self.dataset)
         lengths = (num_data * frac_list).astype(int)
         lengths[-1] = num_data - np.sum(lengths[:-1])
 
         if self.data_config["shuffle"]:
-            indices = np.random.RandomState(
-                seed=None).permutation(num_data)
+            indices = np.random.RandomState(seed=None).permutation(num_data)
         else:
             indices = np.arange(num_data)
 
