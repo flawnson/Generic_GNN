446ef3a04db91b6855b3c67e6e9bd7a846610753
flawn <flawnsontong1@gmail.com>
2020-07-21 23:08:43 -0400

    Added new TODOs and examples for further implementation

--- main.py
+++ main.py
@@ -6,11 +6,16 @@
     """
 # TODO: Implement logger to log progress of code execution
 # TODO: Implement tuning pipeline and benchmarking pipeline
+# TODO: Implement optimizer customization
+# TODO: Implement basic multi-GPU support
+# TODO: Implement model saving and loading
+# TODO: Implement basic unit testing for splits
 # TODO: Implement VAE to generate dataset features
 # TODO: Implement linear model to validate dataset features
 # TODO: Implement TensorFlow summarywriter for logging training metrics
 # TODO: Create directory tree and requirements.txt with bash script
 import os.path as osp
+import subprocess
 import argparse
 import torch
 import json
@@ -29,13 +34,22 @@ if __name__ == "__main__":
 
     json_data: dict = json.load(open(args.config))
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # See https://discuss.pytorch.org/t/what-does-torch-backends-cudnn-benchmark-do/5936
+    if torch.cuda.is_available():
+        torch.backends.cudnn.benchmark = False
+    else:
+        torch.backends.cudnn.benchmark = True
+
+    # git_hash = subprocess.check_output(["git", "describe", "--always"]).strip().decode()
+    # git_branch = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]).strip().decode()
+    # log.info(f"Git hash: {git_hash}, branch: {git_branch}")
 
     # Use if-else to check if requested dataset and model type (from config file) is available
     if json_data.get("data_config")["category"]:
         dataset: GenericDataset = None
         dataset = PrimaryLabelset(json_data["data_config"]).dataset.to(device)
     else:
-        print(f"{json_data['model']} is not a model")
+        print(f"{json_data['model']} is not a model")  #Add to logger when implemented
 
     dataset.splits = Holdout(json_data["data_config"], dataset, bool_mask=True).split()
 
