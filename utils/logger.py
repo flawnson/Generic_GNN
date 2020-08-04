f779b05c0ad8f896b26faf44999106af9d8229bd
flawnson <flawnsontong1@gmail.com>
2020-08-04 19:40:04 -0400

    Continued/started work on early_stopping, logger, and metrics

---
+++ utils/logger.py
@@ -0,0 +1,9 @@
+import datetime
+import logging
+import os
+
+import os.path as osp
+
+log_format = '[%(levelname)-8s %(asctime)s %(filename)s:%(lineno)d] %(message)s '
+date_format = '%Y-%m-%d %I:%M:%S %p'
+
