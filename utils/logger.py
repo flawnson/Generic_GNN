c32d8473532e4b139b31ec85b90eb1ad8e064ec1
flawnson <flawnsontong1@gmail.com>
2020-08-05 13:39:04 -0400

    Added first draft of logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,5 +1,6 @@
 import datetime
 import logging
+import errno
 import os
 
 import os.path as osp
@@ -7,3 +8,39 @@ import os.path as osp
 log_format = '[%(levelname)-8s %(asctime)s %(filename)s:%(lineno)d] %(message)s '
 date_format = '%Y-%m-%d %I:%M:%S %p'
 
+
+# Callable function to set logger for any module in the repo
+def set_file_logger(name='', filename="run.log", level=logging.DEBUG):
+    logger = logging.getLogger(name)
+    logging.basicConfig(format=log_format,
+                        datefmt=date_format,
+                        level=level)
+    time = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+    try:
+        os.makedirs(osp.join(osp.dirname(__file__), f"../logs"))
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise e
+    log_file = osp.join(osp.dirname(__file__), f"../logs", f"{time}-{filename}")
+    print(f"Writing logs to {log_file}")
+    handler = logging.FileHandler(osp.join(osp.dirname(__file__), f"../logs", f"{time}-{filename}"), mode="a")
+    log_formatter = logging.Formatter(fmt=log_format,
+                                      datefmt=date_format)
+    handler.setFormatter(log_formatter)
+    logger.addHandler(handler)
+    logger.setLevel(level)
+    return logger
+
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    return logger
+
+
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter(log_format, datefmt=date_format)
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
+
+log = get_logger(__name__)
